5d7d93147de2c5d03552d77e71fedabe2454c59d
Stakrotsky <Stakrotsky@gmail.com>
2024-10-26 00:25:24 +0300

    Реализовать страницу Главная

--- src/blog.js
+++ src/blog.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import { Authorization, Registration, Users, Post } from './pages';
+import { Authorization, Registration, Users, Post, Main } from './pages';
 import { Header, Footer, Modal } from './components';
 import { useDispatch } from 'react-redux';
 import { setUser } from './actions';
@@ -43,7 +43,7 @@ export const Blog = () => {
 			<Header />
 			<Page>
 				<Routes>
-					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/" element={<Main />} />
 					<Route path="/login" element={<Authorization />} />
 					<Route path="/register" element={<Registration />} />
 					<Route path="/users" element={<Users />} />
